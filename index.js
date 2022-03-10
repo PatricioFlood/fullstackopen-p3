a6852a3fb24928f147476222022e43864a559029
Patricio <pflood@frba.utn.edu.ar>
2022-03-10 15:29:56 -0300

    repeated name error

--- index.js
+++ index.js
@@ -59,11 +59,15 @@ const generateId = () => {
   app.post('/api/persons', (request, response) => {
     const body = request.body
   
-    if (!body.name || !body.number) {
+    if (!body.name || !body.number) 
       return response.status(400).json({ 
         error: 'content missing' 
       })
-    }
+
+    if(persons.find(person => person.name === body.name))
+      return response.status(409).json({
+        error: 'name must be unique'
+      })
   
     const person = {
         id: generateId(),
